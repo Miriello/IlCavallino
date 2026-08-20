81b60a07a9fa10551e4d3e2cffbb834e738310a0
Francesco Miriello <francescomiriello@Miriello.local>
2026-08-20 18:13:25 +0200

    migliorate le classi DAO, sto cercando di usare polimorfismo nella possibilità che il gestionale possa servire a vendere articoli da diversi da quelli di tavola calda

--- src/DAO/PiattoDAO.java
+++ src/DAO/PiattoDAO.java
@@ -1,4 +1,36 @@
 package DAO;
 
+import Database.DatabaseManager;
+import Item.Allergene;
+import Item.Ingrediente;
+import Item.Piatto;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PiattoDAO {
+
+    public List<Piatto> findAll() {
+
+        List<Piatto> piatti = new ArrayList<>();
+
+        String sql = "SELECT * FROM piatto JOIN ";
+        try {
+            Connection conn = DatabaseManager.getConnessione();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                String nome = rs.getString("nome");
+                int prezzo = rs.getInt("prezzo");
+                int id = rs.getInt("id");
+                List<Ingrediente> ingredienti = IngredienteDAO.findByPiatto(id);
+                piatti.add(new Piatto(nome, prezzo, ingredienti));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(" Errore durante il caricamento ", e);
+        }
+        return piatti;
+    }
 }

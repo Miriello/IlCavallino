642f7dafea635562b239ae32e8a131765da14592
Francesco Miriello <francescomiriello@Miriello.local>
2026-08-24 17:39:32 +0200

    modifiche allo schema dei dati

--- src/Item/OrdineFornitore.java
+++ src/Item/OrdineFornitore.java
@@ -4,12 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class OrdineFornitore {
-    private AccordoFornitura af;
     private Map<Articolo, Integer> listaOrdine;
     private int numeroOrdine;
 
-    public OrdineFornitore(AccordoFornitura af, Map<Articolo,Integer> listaOrdine, int numeroOrdine){
-        this.af=af;
+    public OrdineFornitore(Map<Articolo,Integer> listaOrdine, int numeroOrdine){
         this.listaOrdine=new HashMap<>(listaOrdine);
         this.numeroOrdine=numeroOrdine;
     }
